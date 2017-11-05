5d66154ae85602425e9b8d4abe6079a913e7ec92
Pete Goodwin <github@imekon.org>
2017-11-05 17:36:15 +0000

    added large starfield

--- ship.gd
+++ ship.gd
@@ -18,14 +18,3 @@ func _fixed_process(delta):
 	
 	if (Input.is_action_pressed("ui_thrust")):
 		apply_impulse(Vector2(), Vector2(0, -ACCELERATION).rotated(get_rot()))
-		
-	var pos = get_pos()
-	if (pos.x < 0):
-		pos.x = size.x - 1
-	if (pos.x > size.x):
-		pos.x = 1
-	if (pos.y < 0):
-		pos.y = size.y - 1
-	if (pos.y > size.y):
-		pos.y = 1
-	set_pos(pos)
